55f78e74685999974c32e1293c28593d656a460d
Bartenbach <bbartenbach@chsomaha.org>
2016-06-28 15:13:45 -0500

    fixes

--- Get-Menu.ps1
+++ Get-Menu.ps1
@@ -6,13 +6,12 @@ function CheckPSVersion() {
   if ($PSVersionTable.PSVersion.Major -lt 3.0) {
     Write-Host "Your PowerShell is incapable of this feature.  Update it." -ForegroundColor "Yellow"
     Write-Host "https://www.microsoft.com/en-us/download/confirmation.aspx?id=50395" -ForegroundColor "Blue"
-    $null = Read-Host
   }
 }
 
 function Download-Menu() {
 	$url = "http://myportal.chsomaha.org/documents/departments/food%20service/cafeteria%20menu.doc"
-	$output = "$HOME\Desktop\Menu.doc"
+	$output = "$HOME\Menu.doc"
 	Invoke-WebRequest -Uri $url -OutFile $output
 }
 
@@ -21,9 +20,14 @@ function main() {
   CheckPSVersion
   Write-Host "Downloading Menu..." -ForegroundColor "Green"
   Download-Menu
-	Start-Sleep -s 2
-	Write-Host "Launching..." -ForegroundColor "Green"
-	& 'C:\Program Files (x86)\Microsoft Office\Office14\WINWORD.EXE' "$HOME\Desktop\Menu.doc"
+  Start-Sleep -s 2
+  Write-Host "Launching..." -ForegroundColor "Green"
+  & 'C:\Program Files (x86)\Microsoft Office\Office14\WINWORD.EXE' "$HOME\Menu.doc"
+  Write-Host "Waiting for process to exit..." -ForegroundColor "Green"
+  Wait-Process WINWORD
+  Write-Host "Removing Menu from Desktop..." -ForegroundColor "Green"
+  rm "$HOME\Menu.doc"
+  Write-Host "Done!" -ForegroundColor "Green"
 }
 
 main
--- Menu.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mode con:cols=82 lines=37
-
-powershell -ExecutionPolicy Bypass .\Get-Menu.ps1
